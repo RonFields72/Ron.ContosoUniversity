140df14a8c1a7692106f9bc4ca8c3ec7a67cd5ee
Ron Fields <eightbits72@gmail.com>
2014-06-24 21:45:26 -0500

    Initial commit.

---
+++ Ron.ContosoUniversity/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Ron.ContosoUniversity
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Ron.ContosoUniversity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ron.ContosoUniversity.MvcApplication" Language="C#" %>
